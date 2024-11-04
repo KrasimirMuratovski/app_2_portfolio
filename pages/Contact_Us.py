8f28dd8dc3937f29f73da814263fe12d5b0bcabb
kraskoval <kr.muratovski@gmail.com>
2024-11-04 12:47:02 +0200

    Day 23: Final commit; env var pass issue.

--- pages/Contact_Us.py
+++ pages/Contact_Us.py
@@ -1,17 +1,21 @@
 import streamlit as st
 
 from send_email import send_email
+import pandas as pd
 
 st.header("Contact me")
-
+df = pd.read_csv("004 topics.csv")
 with st.form(key="email_form"):
 	user_email = st.text_input("Your email address")
+	drop_menu=st.selectbox(
+		"What topic do you want to discuss",
+		df["topic"])
 	raw_message = st.text_area("Your message")
-	message = f"""
+	message = f"""\
 	Subject: New email from {user_email}
 	
 	From: {user_email}
-	{raw_message}
+	Topic {drop_menu}
 """
 	button = st.form_submit_button("Submit")
 	if button:
--- send_email_me.py
+++ send_email_me.py
@@ -1,14 +1,13 @@
 import smtplib, ssl
-import os
 
-def send_email(message):
+
+def send_email_me(message):
 	host = "smtp.gmail.com"
 	port = 465
-	username = "koynarecam@gmail.com"
-	password = os.getenv("PASSWORD")
+	username = 'koynarecam@gmail.com'
+	password = "bhmh atbl pmkh scug"
 	receiver = "fragmantica.django@gmail.com"
 	context = ssl.create_default_context()
 	with smtplib.SMTP_SSL(host, port, context=context) as server:
 		server.login(username, password)
 		server.sendmail(username, receiver, message)
-
