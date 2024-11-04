304fbfa34edd205cef99eabb1409d0d1a10d1f98
kraskoval <kr.muratovski@gmail.com>
2024-11-04 11:18:40 +0200

    Day 23: send email using the form.

--- send_email.py
+++ send_email.py
@@ -1,16 +1,14 @@
 import smtplib, ssl
 
-host = "smtp.gmail.com"
-port = 465
 
-username = "koynarecam@gmail.com"
-password = "bhmh atbl pmkh scug"
+def send_email(message):
+	host = "smtp.gmail.com"
+	port = 465
+	username = "koynarecam@gmail.com"
+	password = "bhmh atbl pmkh scug"
+	receiver = "fragmantica.django@gmail.com"
+	context = ssl.create_default_context()
+	with smtplib.SMTP_SSL(host, port, context=context) as server:
+		server.login(username, password)
+		server.sendmail(username, receiver, message)
 
-receiver = "fragmantica.django@gmail.com"
-
-context = ssl.create_default_context()
-
-with smtplib.SMTP_SSL(host, port, context=context) as server:
-	server.login(username, password)
-	server.sendmail(username, receiver, f"Subject: Hi! "
-										f"Hello, {receiver}")
